a7b1678c7e6275776961611310b3915916e47d2a
diogonando <diogonando@gmail.com>
2018-08-11 01:11:06 -0300

    Firefox em container com starter para macos
    Primeira versao do firefox para macos isolado em um container docker

---
+++ firefox/run.sh
@@ -0,0 +1,11 @@
+#Obtem IP para cadastrar no xhosts
+IP=`ifconfig -a | grep -A 3 en0: | tail -1 | awk '{print $2}'`
+#garante que DISPLAY esta local
+export DISPLAY=:0
+#Cadastra xhosts
+xhost + $IP
+#Executa firefox exportando display
+docker run -ti --rm \
+       -e DISPLAY=$IP:0 \
+       -v /tmp/.X11-unix:/tmp/.X11-unix \
+       firefox
